9e07efb3ef4aa09bc319ae7eb125bcf848ac42f3
k7lim <k7lim@cs.stanford.edu>
2023-01-30 14:24:06 -0800

    learning named vs default components via https://beta.reactjs.org/learn/importing-and-exporting-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Gallery from './Gallery.js';
+import { Profile } from './Gallery.js';
 
 export default function App() {
   return (
+    <section>
     <Gallery />
+
+    <Gallery />
+
+    <Profile />
+    </section>
   );
 }
