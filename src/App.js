067a3123f5dd1243fbb39b037a0afdba71f8fb0f
k7lim <k7lim@cs.stanford.edu>
2023-01-31 09:21:50 -0800

    finishing lesson on component import/export

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Gallery from './Gallery.js';
-import { Profile } from './Gallery.js';
+import Profile from './Profile.js';
 
 export default function App() {
   return (
-    <section>
-    <Gallery />
-
-    <Gallery />
-
-    <Profile />
-    </section>
+    <div>
+      <Profile />
+      <Gallery />
+    </div>
   );
 }
