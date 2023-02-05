286c7750c5dc71f8faef18d5c71fcce39376232d
k7lim <k7lim@cs.stanford.edu>
2023-02-04 18:28:08 -0800

    learning react basics, props, via https://beta.reactjs.org/learn/passing-props-to-a-component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { saveAnswers } from './redux/actions/answerActions';
+import { getImageUrl } from './utils.js';
+
+
+function Avatar({ person, size }) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
 
 const App = () => {
   const [answers, setAnswers] = useState({});
@@ -53,6 +67,29 @@ const App = () => {
           Submit
         </button>
       </form>
+      <div>
+      <Avatar
+        size={100}
+        person={{ 
+          name: 'Katsuko Saruhashi', 
+          imageId: 'YfeOqp2'
+        }}
+      />
+      <Avatar
+        size={80}
+        person={{
+          name: 'Aklilu Lemma', 
+          imageId: 'OKS67lh'
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{ 
+          name: 'Lin Lanying',
+          imageId: '1bX5QH6'
+        }}
+      />
+    </div>
     </div>
   );
 };
